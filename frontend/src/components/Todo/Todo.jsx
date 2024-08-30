6d1b88542cfdfadc01350835d402a59c8a5b5839
Shreyans T <shreyans.tatiya@gmail.com>
2024-08-30 15:57:48 +0530

    frontend checkpoint

--- frontend/src/components/Todo/Todo.jsx
+++ frontend/src/components/Todo/Todo.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from './styles'
 import Form from '../Form/Form';
+import axios from '../../axios';
+import TodoList from '../TodoList/TodoList';
 
 function Todo() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/todos");
+      setTodos(response.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+  const addTodo = async (e) => {
+    e.preventDefault();
+    if (input.length === 0) return null;
+    await axios.post("/todos", [{
+      ...todos,
+      text: input,
+      completed: false
+    }])
+    fetchData();
+    setInput('')
+  }
+
+  console.log(todos, "todos");
+
   return (
     <Container>
       <h2>List of Todos</h2>
-      <Form />
-      {/* Todo List
-      Key Component
+      <Form input={input} setInput={setInput} addTodo={addTodo} />
+      {/* TodoList Component */}
+      {/* Key Component
       Author Component */}
     </Container>
   )
