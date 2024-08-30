6136a508b5bb7bc3497c722f79a010b2800b197d
Shreyans T <shreyans.tatiya@gmail.com>
2024-08-30 15:04:54 +0530

    backend complete

---
+++ backend/controllers/TodoController.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Todos = require('../models/Todos');
+
+const getTodos = async (req, res) => {
+    try {
+        const allTodos = await Todos.find({}).sort({ createdAt: -1 });
+        res.status(200).send(allTodos);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const createTodo = async (req, res) => {
+    const dbTodo = req.body;
+    try {
+        const newTodo = await Todos.create(dbTodo);
+        res.status(201).send(newTodo);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+
+const updateTodo = async (req, res) => {
+    const { id } = req.params;
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).send(`No todo with that id ${id}`);
+        }
+        const todoId = { _id: id };
+        const update = { completed: true };
+            const updateTodos = await Todos.findOneAndUpdate(todoId, update);
+            if (!updateTodos) {
+                return res.status(404).send(`No todo with that id ${id}`);
+            }
+        res.status(200).send(updateTodos);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+const deleteTodo = async (req, res) => {
+    const { id } = req.params;
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).send(`No todo with that id ${id}`);
+        }
+        const deleteTodos = await Todos.findOneAnddelete({ _id: id });
+        res.status(200).send(deleteTodos);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+module.exports = {
+    getTodos,
+    createTodo,
+    updateTodo,
+    deleteTodo
+}
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { db } = require('./db/db');
+
+dotenv.config();
+
+const { getTodos, createTodo, updateTodo, deleteTodo } = require('./controllers/TodoController');
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+
+PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    db();
+    console.log(`Server is running on port ${PORT}`);
+}) 
+
+
+// API Endpoints
+app.get('/todos', getTodos);
+app.post('/todos', createTodo);
+app.put('/todos/:id', updateTodo);
+app.delete('/todos/:id', deleteTodo);
