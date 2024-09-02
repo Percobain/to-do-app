34045aaf57cd7ccdf93f051f879468b4a4fba3f3
Shreyans T <shreyans.tatiya@gmail.com>
2024-09-02 20:18:54 +0530

    damn

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://to-do-app-server-tawny.vercel.app/',
+    baseURL: 'https://to-do-app-server-tawny.vercel.app',
 });
 
 export default instance;
